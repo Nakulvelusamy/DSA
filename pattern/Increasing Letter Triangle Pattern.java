79fb9b1e3f5029993d2b8764a48db33b48631069
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-16 18:13:40 +0530

    Create Increasing Letter Triangle Pattern.java

---
+++ pattern/Increasing Letter Triangle Pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      patern(6);
+  
+    }
+      static void patern(int n){
+        
+        for (int row=0;row<=n;row++){
+          char l = 'A';
+          for (int col=0;col<=row;col++){
+          System.out.print(l+" ");
+          l++;
+          }
+          
+          System.out.println();
+        }
+      }
+  }
