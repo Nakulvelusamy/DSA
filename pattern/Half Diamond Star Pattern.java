b086b5d8ce24468867911d63d8cba3bc021d30cc
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-16 17:34:07 +0530

    Create Half Diamond Star Pattern.java

---
+++ pattern/Half Diamond Star Pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      patern(6);
+  
+    }
+      static void patern(int n){
+        int l;
+        for (int row=-n;row<=n;row++){
+          if(row<0){
+            l=-row;
+          }
+          else{
+            l=row;
+          }
+          for (int space=0;space<n-l;space++) {
+             System.out.print("* ");
+          }
+          for (int col=0;col<l+1;col++){
+              System.out.print("  ");
+          }
+          System.out.println();
+        }
+      }
+  }
