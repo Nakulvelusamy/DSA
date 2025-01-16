5ca2c8d79f9b2178cad3dacce054f2ebf6083a6b
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-16 17:22:14 +0530

    Create left mirror image pattern.java

---
+++ pattern/left mirror image pattern.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      patern(6);
+  
+    }
+      static void patern(int n){
+        int l;
+        for (int row=-n;row<n;row++){
+          if(row<0){
+            l=-row;
+          }
+          else{
+            l=row;
+          }
+          
+          for (int col=0;col<l+1;col++){
+              System.out.print("* ");
+          }
+          System.out.println();
+        }
+      }
+  }
