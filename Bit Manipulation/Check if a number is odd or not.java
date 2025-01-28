6e6754c13a927436c1404647db09db5485ef602b
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-28 13:40:56 +0530

    Create Check if a number is odd or not.java

---
+++ Bit Manipulation/Check if a number is odd or not.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a=4;
+        if((a&1)==0){
+            System.out.print("Even");
+        }
+        else{
+            System.out.print("Odd");
+        }
+        
+    }
+}
