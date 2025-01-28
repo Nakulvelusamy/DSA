119d067622705df4998f885b51cb2b0b404b2013
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-28 14:22:19 +0530

    Create Unset the rightmost unset bit.java

---
+++ Bit Manipulation/Set/Unset the rightmost unset bit.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int number=4;
+        int n=number|(number+1);
+        int n1=number&(number-1);
+        System.out.print("Set : "+n+"\n"+"Unset : "+n1);
+        
+    }
+}
