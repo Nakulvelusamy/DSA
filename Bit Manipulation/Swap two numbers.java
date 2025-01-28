0051eb331e4d373b54f3a69b327ad7883ee492e7
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-28 13:34:51 +0530

    Create Swap two numbers.java

---
+++ Bit Manipulation/Swap two numbers.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.print(a+" "+b);
+    }
+}
