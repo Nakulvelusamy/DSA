bbb7a2e873311db440fa34ea9ff3e73ce93af99e
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-01-28 15:02:18 +0530

    Delete Bit Manipulation/Set directory

--- Bit Manipulation/Set/Unset the rightmost unset bit.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        int number=4;
-        int n=number|(number+1);
-        int n1=number&(number-1);
-        System.out.print("Set : "+n+"\n"+"Unset : "+n1);
-        
-    }
-}
