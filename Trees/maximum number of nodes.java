13c38b03b6f9780266246ee0033b6d5ed9643df8
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-03-27 09:29:32 +0530

    Create maximum number of nodes.java

---
+++ Trees/maximum number of nodes.java
@@ -0,0 +1,9 @@
+class Solution {
+    static int countNodes(int i) {
+        // // code here
+        // Scanner sc=new Scanner(System.in);
+        // int n=sc.nextInt();
+        int ans =(int) Math.pow(2,i-1);
+        return ans;
+    }
+}
