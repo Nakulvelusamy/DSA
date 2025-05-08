bec6ecdb4bf2a186163e02eadd0aa75355637512
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-05-08 14:27:45 +0530

    Update All Divisors of a Number.java

--- advanced math/All Divisors of a Number.java
+++ advanced math/All Divisors of a Number.java
@@ -1,9 +1,13 @@
-public static void printDivisors(int n) {
-    for (int i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i == 0) {
-            System.out.print(i + " ");
-            if (i != n / i) {
-                System.out.print((n / i) + " ");
+public class AllDivisors {
+    public static void main(String[] args) {
+        int n = 60;
+        System.out.print("All Divisors of " + n + ": ");
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                if (i != n / i) {
+                    System.out.print((n / i) + " ");
+                }
             }
         }
     }
