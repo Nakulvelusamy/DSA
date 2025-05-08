cd6203fbe185919a5a8080a46c91b28ee600a176
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-05-08 14:25:19 +0530

    Create All Divisors of a Number.java

---
+++ advanced math/All Divisors of a Number.java
@@ -0,0 +1,10 @@
+public static void printDivisors(int n) {
+    for (int i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            System.out.print(i + " ");
+            if (i != n / i) {
+                System.out.print((n / i) + " ");
+            }
+        }
+    }
+}
