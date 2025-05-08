39249f2795fff38eefe7b8fc0c04719de0927917
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-05-08 14:24:48 +0530

    Create Print Prime Factors of a Number.java

---
+++ advanced math/Print Prime Factors of a Number.java
@@ -0,0 +1,11 @@
+public static void primeFactors(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            System.out.print(i + " ");
+            n /= i;
+        }
+    }
+    if (n > 1) {
+        System.out.print(n);
+    }
+}
