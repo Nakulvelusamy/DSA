14792ec6d0b44fcd5bb84541a59658203fa026e3
Nakul V <157946103+Nakulvelusamy@users.noreply.github.com>
2025-05-08 14:28:06 +0530

    Update Sieve of Eratosthenes.java

--- advanced math/Sieve of Eratosthenes.java
+++ advanced math/Sieve of Eratosthenes.java
@@ -1,19 +1,25 @@
-public static void sieve(int n) {
-    boolean[] isPrime = new boolean[n + 1];
-    Arrays.fill(isPrime, true);
-    isPrime[0] = isPrime[1] = false;
+import java.util.Arrays;
 
-    for (int i = 2; i * i <= n; i++) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= n; j += i) {
-                isPrime[j] = false;
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        int n = 60;
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-    }
 
-    for (int i = 2; i <= n; i++) {
-        if (isPrime[i]) {
-            System.out.print(i + " ");
+        System.out.print("Primes up to " + n + ": ");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
